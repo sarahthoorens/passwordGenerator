363c800b4a43ad9aa0e14ee230f1563318c3336e
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-09-28 22:35:22 -0500

    password value now matches console log. Will need to update passwordString() to allow for some characters to be excluded.

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,47 @@
 // Assignment Code - creates a reference to the generate button
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button - tell generate button, when it's click, 
-//run the writepassword function
+// Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input - creates a variable 
-//called password that's assigned to a function called generate password.
+// Write password to the #password input
 function writePassword() {
   var chars = prompt('How many characters would you like your password to contain?');
-if (chars >= 8 || chars <= 128) {
-  generatePassword();
-} else {
-  alert("Your number must be between 8 and 128.");
-};
+  if (chars >= 8 || chars <= 128) { // This code is not working - currently # of chars is unlimited
+    generatePassword();
+  } else {
+    alert("Your number must be between 8 and 128.");
+  };
   var password = passwordString(chars);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-}
+  console.log(password); 
+  }
 
+// Prompts user to choose what characters to include in the password 
 function generatePassword() {
   var specialChar = confirm('Click OK to confirm including special characters.');
   var lowercase = confirm('Click OK to confirm including lowercase characters?');
   var uppercase = confirm('Click OK to confirm including uppercase characters?');
   var numbers = confirm('Click OK to confirm including numbers?');
   var userInput = [specialChar, lowercase, uppercase, numbers];
-    passwordString();
-  }
+  passwordString();
 
+}
+// Creates the random string based on chars input and length
+// Need to adjust this code to allow for userInput elements to be False (i.e. no special characters, etc.)
 function passwordString(length, chars) {
   var result = '';
-  var charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  for (var i = 0; i < length; i++) 
+  var charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_'
+                                  // var num = '0123456789';
+                                  // var lcase = 'abcdefghijklmnopqrstuvwxyz'
+                                  // var ucase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                                  // var char = '!@#$%^&*()_'
+                                  // var charset = [num,lcase, ucase, char];
+for (var i = 0; i < length; i++)
     result += charset[Math.floor(Math.random() * charset.length)];
-    return result;
+  return result;
 }
-
-
-  // return userInput;
-
-// var chars = prompt('How many characters would you like your password to contain?');
-// if (chars >= 8 || chars <= 128) {
-//   generatePassword();
-// } else {
-//   alert("Your number must be between 8 and 128.");
-// };
-
-
-
-// console.log(passwordString(chars))
-
-
-
-// var randomString = passwordString[chars, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 
 
 
